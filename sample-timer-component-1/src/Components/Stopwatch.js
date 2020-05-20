8c3672fa8dd1fd01dc74d84f4a3182427cc83e97
CodingCCarpenter <christineassists@aol.com>
2020-05-19 21:39:36 -0500

    set up stopwatch stop and reset logic

--- sample-timer-component-1/src/Components/Stopwatch.js
+++ sample-timer-component-1/src/Components/Stopwatch.js
@@ -8,7 +8,7 @@ const Stopwatch =  () => {
         timerTime: 0
     };
 
-    // in the below function startTimer, setState is used to turn the timer on, set the timer to represent the current time, and initialize the start time. Subtracting this.state.timerTime from Date.now() will set start time to either when the timer was started, or what that time would have been if the timer is resumed.
+    // in the startTimer method, setState is used to turn the timer on, set the timer to represent the current time, and initialize the start time. Subtracting this.state.timerTime from Date.now() will set start time to either when the timer was started, or what that time would have been if the timer is resumed.
     
     // next we initialize a timer interval with this.timer to set the timer interval to the Stopwatch component to return a method to call every time it goes off, and an interval time. In the return we call this.setState to adjust timerTime to the number of miliseconds since timerStart
 
@@ -24,6 +24,15 @@ const Stopwatch =  () => {
             });
         }, 10) ;
     };
+
+    // in the stopTimer method, we set timerOn to false and clear the interval on this.timer
+
+    const stopTimer = () => {
+        this.setState({ timerOn: false });
+        clearInterval(this.timer);
+    };
+
+    //in the resetTimer method, we set the timerStart and timerTime back to 0.
 
     return (
         <div className='Stopwatch'>
