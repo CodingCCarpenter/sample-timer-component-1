b87c1076a8be88f9abeae2805d38388d0179f651
CodingCCarpenter <christineassists@aol.com>
2020-05-19 21:09:22 -0500

    clean up imports from app.jsa and add new components

---
+++ sample-timer-component-1/src/Components/Countdown.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css';
+
+const Countdown =  () => {
+    return (
+        <div className='Countdown'>
+            <div className='Countdown-header'>Countdown</div>
+        </div>
+    );
+}
+
+export default Countdown;
---
+++ sample-timer-component-1/src/Components/Stopwatch.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css';
+
+const Stopwatch =  () => {
+    return (
+        <div className='Stopwatch'>
+            <div className='Stopwatch-header'>Stopwatch</div>
+        </div>
+    );
+}
+
+export default Stopwatch;
